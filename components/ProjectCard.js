50f30999b330bb198c4628d255b21439982cde03
alihany <alihanyiilmaz@gmail.com>
2020-10-30 21:45:26 +0300

    edit projectcard component

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -12,9 +12,9 @@ import {
 
 const trackGoal = (title) => {
   const goalCodes = {
-    'React 2025': '5WGDOKV0',
-    'Mastering Next.js': 'HV9HDL0O',
-    jamstackfns: 'LRXBPLZS'
+    'Twitter React Clone': '5WGDOKV0',
+    'COVID-19 Tracker': 'HV9HDL0O',
+    'Frontend Mentor': 'LRXBPLZS'
   };
 
   Fathom.trackGoal(goalCodes[title], 0);
